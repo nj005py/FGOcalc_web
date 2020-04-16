4ca1e7708d363a06df5d1b92104a3c8c2c8212dd
nj005py <py5555@hotmail.com>
2020-04-16 22:57:48 +0800

    update

--- src/components/GameStrategy.js
+++ src/components/GameStrategy.js
@@ -1,16 +1,17 @@
 export default `
-# FGO国服《旧时蜘蛛·余残怀古·共纺丝》白情攻略搜集
+# FGO国服《德川回天迷宫大奥》攻略
 
 [toc]
 
 ## B站削人棍
-- [视频攻略](https://www.bilibili.com/video/BV1Ec411h7QL)
+- [视频攻略](https://www.bilibili.com/video/BV1m64y1u76v)
+- [图文攻略](https://www.bilibili.com/read/cv5636394)
 
 ## NGA攻略流程
 
 ## B站叫我棉被
-- [材料掉落](https://i0.hdslb.com/bfs/album/6d6a3a3239d66d314d473ca1521bfcd3c95d5d4b.png@518w_1e_1c.png)
-- [视频攻略](https://www.bilibili.com/video/BV1x741117fg)
+- [材料掉落](https://space.bilibili.com/6014992/dynamic)
+- [视频攻略](https://www.bilibili.com/video/BV1Ua4y1t7jF)
 
 ## 攻略分布
 
